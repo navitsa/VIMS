d1507e5e10204d25ecb2e8d0a74d0a9ca8530852
indraBandara <61151535+indraBandara@users.noreply.github.com>
2021-06-14 16:36:03 +0530

    Update EquipmentMasterRepository.java

--- VIMS/src/main/java/com/navitsa/repository/EquipmentMasterRepository.java
+++ VIMS/src/main/java/com/navitsa/repository/EquipmentMasterRepository.java
@@ -31,18 +31,32 @@ public interface EquipmentMasterRepository extends CrudRepository<EquipmentMaste
 	@Query(value="SELECT em FROM EquipmentMaster em WHERE em.nextCalibrationDate between :nextCaliDate and concat(YEAR(:nextCaliDate),'-12','-31') and  em.invLoID.centerID.center_ID=:center and em.status='ACTIVE'")
 	public List<EquipmentMaster> equmentCalibrationReport(@Param("nextCaliDate")String nextCaliDate,@Param("center")String center);
 
-	/*
-	 * @Query(
-	 * value="SELECT equipment_master.*, test_lanes_details.testLaneHead_Id\n" +
-	 * "FROM equipment_master\n" +
-	 * "LEFT JOIN test_lanes_details ON equipment_master.Equipment_ID = test_lanes_details.Equipment_ID\n"
-	 * +
-	 * "WHERE test_lanes_details.Center_ID=:center and equipment_master.Status='ACTIVE'"
-	 * ,nativeQuery = true) public String[][] equmentCalendar(@Param("center")String
-	 * center);
-	 */
-	@Query(value="SELECT em FROM EquipmentMaster em WHERE em.invLoID.centerID.center_ID=:center and em.status='ACTIVE'")
-	public List<EquipmentMaster> equmentCalendar(@Param("center")String center);
+	@Query(value="SELECT equipment_master.Equipment_ID,\n" + 
+			"equipment_model.Eq_Model_ID,\n" + 
+			"equipment_master.Serial_No,\n" + 
+			"equipment_master.Status,\n" + 
+			"equipment_master.Last_Calibration_Date,\n" + 
+			"equipment_master.Last_Service_Date,\n" + 
+			"equipment_master.Next_Calibration_Date,\n" + 
+			"equipment_master.Next_Service_Date,\n" + 
+			"equipment_master.Install_Date,\n" + 
+			"equipment_master.Comm_Date,\n" + 
+			"equipment_master.Train_Date,\n" + 
+			"equipment_master.invLoID,\n" + 
+			"test_lanes_details.testLaneHead_Id,\n" + 
+			"test_lanes_details.Center_ID,\n" + 
+			"test_lane_head.Lane_Name,\n" + 
+			"equipment_type.Eq_Type\n" + 
+			"FROM equipment_master\n" + 
+			"LEFT JOIN test_lanes_details ON equipment_master.Equipment_ID = test_lanes_details.Equipment_ID\n" + 
+			"LEFT JOIN test_lane_head ON test_lanes_details.testLaneHead_Id = test_lane_head.testLaneHead_Id\n"+
+			"LEFT JOIN equipment_model ON equipment_master.Eq_Model_ID = equipment_model.Eq_Model_ID\n"+
+			"LEFT JOIN equipment_type ON equipment_model.Eq_Type_ID = equipment_type.Eq_Type_ID\n"+
+			"WHERE test_lanes_details.Center_ID=:center and equipment_master.Status='ACTIVE'",nativeQuery = true)
+	public String[][] equmentCalendar(@Param("center")String centerID);
+	
+	
+	
 	/*
 	 * @Query(
 	 * value="SELECT em as EquipmentMaster,TestLaneDetails.testLaneDetailsid FROM EquipmentMaster em left join TestLaneDetails on EquipmentMaster.equipmentID=TestLaneDetails.equipmentID WHERE em.invLoID.centerID.center_ID=:center and em.status='ACTIVE'"
